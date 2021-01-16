e7e4f7f762b45c28baf5f29eaacaa7e699619ee5
Stas_Krasnagir <stas220693@gmail.com>
2021-01-16 23:05:55 +0300

    16-01-21

--- Cards.py
+++ Cards.py
@@ -1,1 +1,32 @@
-#123
+import random
+
+
+class Card(object):
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        return str(self.value) + self.suit
+
+
+class StandardDeck(object):
+    def __init__(self):
+        self.cards = []
+        suits = ["H", "S", "D", "C"]
+        values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10,
+                  "Jack": 11, "Queen": 12, "King": 13, "Ace": 14}
+
+        for name in values:
+            for suit in suits:
+                self.cards.append(Card(values[name], suit))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+        print("Deck Shuffled")
+
+
+test = StandardDeck()
+test.shuffle()
+print(test.cards)
+print(len(test.cards))
